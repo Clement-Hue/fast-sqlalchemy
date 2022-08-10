a88e2f96430aa6b8623156e9d69e145265c1775f
Clement_Hue <clementhue@hotmail.fr>
2022-08-10 23:14:34 +0200

    Add event bus system

---
+++ fast_alchemy/event_bus/middleware.py
@@ -0,0 +1,29 @@
+from typing import Iterable
+
+from starlette.types import ASGIApp, Scope, Receive, Send
+
+from fast_alchemy.event_bus import event_bus_store
+from fast_alchemy.event_bus.contexts import event_queue_ctx
+from fast_alchemy.event_bus.emit import publish_events
+
+
+class EventBusMiddleware:
+    def __init__(self, app: ASGIApp, handlers: Iterable):
+        self.app = app
+        self.register_handlers(handlers=handlers)
+
+    def register_handlers(self, handlers: Iterable):
+        event_bus_store.append(*handlers)
+
+    def __del__(self):
+        event_bus_store.clear()
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] not in ["http", "websocket"]:
+            await self.app(scope, receive, send)
+            return
+
+        with event_queue_ctx():
+            await self.app(scope, receive, send)
+            await publish_events()
+
---
+++ tests/event_bus/test_middleware.py
@@ -0,0 +1,24 @@
+import pytest
+from pytest_mock import MockerFixture
+
+from fast_alchemy.event_bus import local_event_bus, event_bus_store
+from fast_alchemy.event_bus.middleware import EventBusMiddleware
+
+
+def test_register_handlers(mocker: MockerFixture):
+    assert len(event_bus_store) == 0
+    middleware = EventBusMiddleware(mocker.MagicMock(), [local_event_bus])
+    assert len(event_bus_store) == 1
+    assert event_bus_store[0] == local_event_bus
+
+@pytest.mark.asyncio
+async def test_publish_events(mocker: MockerFixture):
+    publish_mock = mocker.patch("fast_alchemy.event_bus.middleware.publish_events")
+    assert len(event_bus_store) == 0
+    event_bus = mocker.AsyncMock()
+    middleware = EventBusMiddleware(mocker.AsyncMock(), [event_bus])
+    assert len(event_bus_store) == 1
+    await middleware({"type": "http"}, mocker.MagicMock(),mocker.MagicMock())
+    publish_mock.assert_called()
+
+
