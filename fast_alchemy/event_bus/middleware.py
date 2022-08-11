348849db1a48ec2e39b98c3d865fe792ea647523
Clement_Hue <clementhue@hotmail.fr>
2022-08-12 00:43:07 +0200

    Rename handlers to buses

--- fast_alchemy/event_bus/middleware.py
+++ fast_alchemy/event_bus/middleware.py
@@ -1,8 +1,9 @@
 import logging
 from typing import Iterable
 
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.types import ASGIApp, Scope, Receive, Send
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.types import ASGIApp
 
 from fast_alchemy.event_bus.contexts import event_queue_ctx, event_bus_store
 from fast_alchemy.event_bus.emit import publish_events
@@ -10,14 +11,14 @@ logger = logging.getLogger(__name__)
 
 
 class EventBusMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app: ASGIApp, handlers: Iterable):
+    def __init__(self, app: ASGIApp, buses: Iterable):
         super().__init__(app)
-        self.register_handlers(handlers=handlers)
+        self.register_buses(buses=buses)
 
-    def register_handlers(self, handlers: Iterable):
-        event_bus_store.add(*handlers)
+    def register_buses(self, buses: Iterable):
+        event_bus_store.add(*buses)
 
-    async def dispatch(self, request, call_next):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
         with event_queue_ctx():
             response = await call_next(request)
             await publish_events()
