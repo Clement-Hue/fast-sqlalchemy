d00747657f58a21a9e1b3944b3041bd25a394bc9
Clement_Hue <clementhue@hotmail.fr>
2022-08-13 16:02:11 +0200

    Parse environment variable in yaml file

--- fast_alchemy/config/yaml.py
+++ fast_alchemy/config/yaml.py
@@ -1,10 +1,24 @@
-import confuse, os
+import confuse, os,re, logging
 
+logger = logging.getLogger(__name__)
 
 class Configuration:
     def __init__(self, config_dir=None):
         self.config_dir = config_dir
         self._confuse = None
+        self.yaml_loader = confuse.yaml_util.Loader
+        self.env_pattern = re.compile(r".*?\${(.*?)}")
+        self.yaml_loader.add_implicit_resolver("!pathex", self.env_pattern ,None)
+        self.yaml_loader.add_constructor("!pathex", self.env_constructor)
+
+    def env_constructor(self, loader, node):
+        value = loader.construct_scalar(node)
+        for group in self.env_pattern.findall(value):
+            if env := os.getenv(group):
+                value = value.replace(f"${{{group}}}", env)
+            else:
+                logger.warning(f"Environment variable {group} not found")
+        return value
 
     def load_config(self):
         sources = [confuse.YamlSource(os.path.join(self.config_dir, file))
--- tests/config/test_yaml.py
+++ tests/config/test_yaml.py
@@ -1,9 +1,21 @@
 import os
+
+import pytest
+
 from fast_alchemy.config.yaml import Configuration
+from dotenv import load_dotenv
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_get_yaml_config():
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    config = Configuration(os.path.join(root_dir, "test_cfg_dir"))
-    config.load_config()
-    assert config["db"].get() == "test"
+@pytest.fixture
+def config_test():
+    return Configuration(os.path.join(root_dir, "test_cfg_dir"))
+
+def test_get_yaml_config(config_test):
+    config_test.load_config()
+    assert config_test["db"].get() == "test_db"
+def test_yaml_config_with_env(config_test):
+    load_dotenv(os.path.join(root_dir, ".env"))
+    config_test.load_config()
+    assert config_test["env_key"].get() == "var_foo.bar"
