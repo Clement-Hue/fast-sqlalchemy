1543f61bf2edb2ba7c1b7abf2b1005358d8173a0
Clement_Hue <clementhue@hotmail.fr>
2022-09-21 20:52:17 +0200

    Cli: new command generate boilerplate

---
+++ fast_sqlalchemy/cli/boilerplate/settings.py
@@ -0,0 +1,11 @@
+import sys, os, logging.config
+from fast_sqlalchemy.config.yaml import Configuration
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+config = Configuration(os.path.join(ROOT_DIR, "config"), env_path=os.path.join(ROOT_DIR, ".env"))
+config.load_config(config="test" if 'pytest' in sys.modules else os.getenv("CONFIG", None))
+logging.config.dictConfig(config.get("logging"))
+
+if config.get("mode") == "prod":
+    logging.raiseExceptions = False
